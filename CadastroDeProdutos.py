1c1eb38115d114a93dbaaee801182a9e30945eb5
leo-vilelela <alvesvilelaster@gmail.com>
2023-06-16 12:34:04 -0300

    Uploading file to repository!

---
+++ CadastroDeProdutos.py
@@ -0,0 +1,300 @@
+
+"""
+if(opcaoAdmin == "1"):
+            array = input("Nome do produto:" ) + input("Valor do produto:")
+"""
+
+
+"""
+# Codigo para receber uma lista de numeros e printa-los na tela
+num_array = list()
+num = input("Enter how many elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(num)):
+    n = input("num :")
+    num_array.append(int(n))    
+print ('ARRAY: ',num_array)
+"""
+
+'''
+lst = []
+
+n = int(input("via"))
+
+for i in range (0,n):
+
+    ele = int(input())
+
+    lst.append(ele)
+
+
+
+print (lst)
+
+'''
+"""
+# Coddigo para criar uma janela com tkinter
+import tkinter
+window=tkinter.Tk()
+button_widget=tkinter.Button(window,text="My first screen using tkinter")
+window.title("Interface")
+
+button_widget.pack()
+tkinter.mainloop()
+"""
+
+'''a="dia"
+b="hoje"
+c="de"
+d="atividade"
+
+print(f"{b} {c} {a} {d}")
+hoje de dia atividade'''
+
+
+                # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+from tkinter import *
+from tkinter import ttk
+import sqlite3
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Image
+import webbrowser
+
+root = Tk()
+
+class relatorios():
+    def printCliente(self):
+        webbrowser.open("relatorio.pdf")
+    
+    def geraRelat(self):
+        self.c = canvas.Canvas("relatorio.pdf")
+        self.codigoRel = self.codigo_entry.get()
+        self.produtoRel = self.produto_entry.get()
+        self.valorRel = self.valor_entry.get()
+        self.descricaoRel = self.descricao_entry.get()
+
+        self.c.setFont("Helvetica-Bold",24)
+        self.c.drawString(200, 790, "Ficha do Produto" )
+        self.c.setFont("Helvetica-Bold",14)
+        self.c.drawString(100, 750, 'Nome: ')        
+        self.c.drawString(100, 720, 'Valor: ')
+        self.c.drawString(100, 690, 'Descrição: ')
+
+        self.c.setFont("Helvetica",14)
+        self.c.drawString(200, 750, self.produtoRel)        
+        self.c.drawString(200, 720, self.valorRel)
+        self.c.drawString(200, 690, self.descricaoRel)
+
+        self.c.rect(20, 630, 550, 200, fill=False, stroke=True)
+
+        
+        self.c.showPage()
+        self.c.save()
+        self.printCliente()
+
+class funcs():
+
+    def limpa_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.produto_entry.delete(0, END)
+        self.valor_entry.delete(0, END)
+        self.descricao_entry.delete(0, END)
+
+    def conecta_db(self):
+        self.conn=sqlite3.connect("LCBL.db")
+        self.cursor=self.conn.cursor()
+
+    def desconecta_db(self):
+        self.conn.close()
+
+    def monta_tabelas(self):
+        self.conecta_db()
+        self.cursor.execute("CREATE TABLE if not exists produtos(produto text, valor real, descrição text )")
+        self.conn.commit()
+        self.desconecta_db()
+
+    def variaveis(self):
+        self.cod=self.codigo_entry.get()
+        self.produto=self.produto_entry.get()
+        self.valor=self.valor_entry.get()
+        self.descricao=self.descricao_entry.get()
+
+    def add_produtos(self):
+        #self.codigo=self.codigo=self.codigo_enrty.get()
+        self.variaveis()
+        self.conecta_db()
+        self.cursor.execute("INSERT INTO produtos (produto, valor, descrição) values (?, ?, ?)",(self.produto, self.valor, self.descricao))
+        self.conn.commit()
+        
+        self.desconecta_db()
+        self.select_lista()
+        self.limpa_tela()
+
+    def select_lista(self):
+        self.l_produto.delete(*self.l_produto.get_children())
+        self.conecta_db()
+        lista=self.cursor.execute("SELECT rowid, * FROM produtos ORDER BY rowid ASC;")
+        for i in lista:
+            self.l_produto.insert("", END, values=i)
+        self.desconecta_db()
+
+    def onDoubleClick(self, event):
+        self.limpa_tela()
+        self.l_produto.selection()
+
+        for n in self.l_produto.selection():
+            col1 ,col2, col3, col4= self.l_produto.item(n, 'values')
+            self.codigo_entry.insert(END,col1)
+            self.produto_entry.insert(END,col2)
+            self.valor_entry.insert(END,col3)
+            self.descricao_entry.insert(END,col4)
+
+    def deletar(self):
+        self.variaveis()
+        self.conecta_db()
+        self.cursor.execute("DELETE FROM produtos WHERE rowid = "+self.cod+" ")
+        self.conn.commit()
+        self.desconecta_db()
+        self.limpa_tela()
+        self.select_lista()
+    
+    def alterar(self):
+        self.variaveis()
+        self.conecta_db()
+        self.cursor.execute("UPDATE produtos SET produto = (?), valor = (?), descrição = (?) WHERE rowid = "+self.cod+" ", (self.produto, self.valor, self.descricao))
+        self.conn.commit()
+        self.desconecta_db()
+        self.select_lista()
+        self.limpa_tela()
+
+    def buscar(self):
+        self.conecta_db()
+        self.l_produto.delete(*self.l_produto.get_children())
+        self.produto_entry.insert(END, '%')
+        nome=self.produto_entry.get()
+        self.cursor.execute("SELECT rowid, * FROM produtos WHERE produto LIKE '%s' ORDER BY produto ASC" % nome)
+        buscanome=self.cursor.fetchall()
+        for i in buscanome:
+            self.l_produto.insert("", END, values=i)
+        self.limpa_tela()
+        self.desconecta_db()
+
+class Application(funcs, relatorios):
+
+    def __init__(self) -> None:
+        self.root = root
+        self.tela()
+        self.frames_da_tela()
+        self.widgets_frame_1()
+        self.lista_frame2()
+        self.select_lista()
+        self.menus()
+        root.mainloop()
+        pass
+
+    def tela(self):
+        self.root.title("Cadastro de Produtos")
+        self.root.configure(background='black')
+        self.root.geometry('800x600')
+        self.root.resizable(True,True)
+        self.root.maxsize(width=1000, height=700)
+        self.root.minsize(width=500, height=300)
+
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root, bg="#B6B4E6" ,bd=4, highlightbackground='gray',highlightthickness=2)
+        self.frame_2 = Frame(self.root, bd=4, highlightbackground='gray',highlightthickness=2)
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46 )
+        self.frame_2.place(relx=0.02, rely=0.50, relwidth=0.96, relheight=0.46 )
+        
+    def widgets_frame_1(self):
+
+        self.bt_limpar = Button(self.frame_1, text= 'Limpar', bd= 6, fg='#003233', font= ('arial',10,'bold'), command= self.limpa_tela )
+        self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_buscar = Button(self.frame_1, text= 'Buscar',  bd= 6, fg='#003233', font= ('arial',10,'bold'), command=self.buscar)
+        self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_inserir = Button(self.frame_1, text= 'Inserir',  bd= 6, fg='#003233',  font= ('arial',10,'bold'), command= self.add_produtos)
+        self.bt_inserir.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_alterar = Button(self.frame_1, text= 'Alterar',  bd= 6, fg='#003233', font= ('arial',10,'bold'), command= self.alterar)
+        self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_apagar = Button(self.frame_1, text= 'Apagar',  bd= 6, fg='#003233', font= ('arial',10,'bold'), command= self.deletar)
+        self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.lb_codigo=Label(self.frame_1, text= "Código", bg="#B6B4E6", fg='#003233', font= ('arial',10,'bold'))
+        self.lb_codigo.place(relx= 0.05 ,rely= 0.05)
+
+        self.codigo_entry=Entry(self.frame_1, bd= 4)
+        self.codigo_entry.place(relx=0.05 ,rely=0.15, relwidth= 0.1)
+
+        self.lb_produto=Label(self.frame_1, text= "Produto", bg="#B6B4E6", fg='#003233', font= ('arial',10,'bold'))
+        self.lb_produto.place(relx= 0.05 ,rely= 0.25)
+
+        self.produto_entry=Entry(self.frame_1, bd= 4)
+        self.produto_entry.place(relx=0.05 ,rely=0.35,relwidth=0.25)
+
+        self.lb_valor=Label(self.frame_1, text= "Valor", bg="#B6B4E6", fg='#003233', font= ('arial',10,'bold'))
+        self.lb_valor.place(relx= 0.05 ,rely= 0.45)
+
+        self.valor_entry=Entry(self.frame_1, bd= 4)
+        self.valor_entry.place(relx=0.05 ,rely=0.55,relwidth=0.25)
+
+        self.lb_descricao=Label(self.frame_1,  text= "Descrição", bg="#B6B4E6", fg='#003233', font= ('arial',10,'bold'))
+        self.lb_descricao.place(relx= 0.05 ,rely= 0.65)
+
+        self.descricao_entry=Entry(self.frame_1, bd= 4)
+        self.descricao_entry.place(relx=0.05 ,rely=0.75,relwidth=0.9)
+
+    def lista_frame2(self):
+        self.l_produto= ttk.Treeview(self.frame_2, height=5, columns=("col1","col2","col3","col4"))
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=("TkDefaultFont", 9, "bold"))
+        
+        self.l_produto.heading("#0", text="")
+        self.l_produto.heading("#1", text="Código")
+        self.l_produto.heading("#2", text="Produto")
+        self.l_produto.heading("#3", text="Valor")
+        self.l_produto.heading("#4", text="Descrição")
+
+        self.l_produto.column("#0", width=1)
+        self.l_produto.column("#1", width=50, anchor="center")
+        self.l_produto.column("#2", width=200, anchor="center")
+        self.l_produto.column("#3", width=125, anchor="center")
+        self.l_produto.column("#4", width=125, anchor="center")
+
+        self.l_produto.place(relx="0.02", rely="0.1", relwidth="0.95", relheight="0.85")
+    
+        self.scrollLista=Scrollbar(self.frame_2, orient="vertical")
+        self.l_produto.configure(yscroll=self.scrollLista.set)
+        self.scrollLista.configure(command=self.l_produto.yview)
+        self.scrollLista.place(relx="0.96", rely="0.1", relwidth="0.02", relheight="0.85")
+        self.l_produto.bind("<Double-1>", self.onDoubleClick)
+
+    def menus(self):
+        menubar= Menu(self.root)
+        self.root.config(menu=menubar)
+        filemenu=Menu(menubar)
+        filemenu2=Menu(menubar)
+
+        def quit():  
+            self.root.destroy()
+
+        menubar.add_cascade(label="Opções", menu= filemenu)
+        menubar.add_cascade(label="Relatórios", menu= filemenu2)
+
+        filemenu.add_command(label="Sair", command=quit)
+        filemenu.add_command(label="Limpa campos", command=self.limpa_tela)
+
+        filemenu2.add_command(label="Ficha do produto", command=self.geraRelat)
+        
+        
+
+    
+Application()
